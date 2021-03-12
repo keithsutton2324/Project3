656c9c67f50fbfab2433c16de7e34a68bee3e884
Your Name <suttonk@bcglobal.net>
2021-03-12 15:02:42 -0800

    preferred language and country added

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,32 +1,35 @@
 import React from "react";
 class Signup extends React.Component {
-    state = {
-        username: "",
-        password: ""
-    }
-    
-    onInputChange = (event) => {
-        var name = event.target.name
-        var value = event.target.value
-        this.setState({
-            [name]: value
-        })
-    }
-    render() {
-        return (<div>
-            <h1>Proj 3</h1>
-            <form action="/submit" method="post">
-                <h2>Create A User</h2>onChage ={this.onInputChange}
-                <input type="text" class="form-control" name={this.state.username} placeholder="Username" autofocus />
-                <input type="text" class="form-control" name={this.state.password} placeholder="Password" autofocus />
-                <input type="text" class="form-control" name={this.state.email} placeholder="Email" />
-                <input type="text" class="form-control" name={this.state.preferredlang} placeholder="Preferred Language" autofocus />
-                <input type="text" class="form-control" name={this.state.country} placeholder="Country" autofocus />
-                <button class="btn btn-lg btn-primary btn-block" id="submit" type="submit">Submit</button>
-            </form>
-        </div>
-        )
-    }
-  }   
+  state = {
+    username: "",
+    password: "",
+    email: "",
+    preferredlang: "",
+    country: ""
+  }
+
+  onInputChange = (event) => {
+    var name = event.target.name
+    var value = event.target.value
+    this.setState({
+      [name]: value
+    })
+  }
+  render() {
+    return (<div>
+      <h1>Project 3</h1>
+      <form action="/submit" method="post">
+        <h2>Create A User</h2>onChange ={this.onInputChange}
+        <input type="text" class="form-control" name={this.state.username} placeholder="Username" autofocus />
+        <input type="text" class="form-control" name={this.state.password} placeholder="Password" autofocus />
+        <input type="text" class="form-control" name={this.state.email} placeholder="Email" autofocus />
+        <input type="text" class="form-control" name={this.state.preferredlang} placeholder="Preferred Language" autofocus />
+        <input type="text" class="form-control" name={this.state.country} placeholder="Country" autofocus />
+        <button class="btn btn-lg btn-primary btn-block" id="submit" type="submit">Submit</button>
+      </form>
+    </div>
+    )
+  }
+}
 
 export default Signup;
--- models/index.js
+++ models/index.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: { type: String, required: true },
+  username: { type: String, required: true },
   password: { type: String, required: true },
+  email: { type: String, required: true },
+  preferredlang: { type: String, required: true },
+  country: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
